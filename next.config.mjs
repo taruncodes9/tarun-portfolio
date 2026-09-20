24bbe57aad7e7d9a650f3f81808d29bd9f73d7e4
Tarun Chhabra <tarunchhabra763@gmail.com>
2026-09-20 19:12:42 +0530

    Cofigure Next.Js Config for Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  basePath: "/tarun-portfolio",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
